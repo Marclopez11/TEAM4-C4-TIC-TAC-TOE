af8a1766046dce7bb891a7bee47b637e789ce1c0
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-05-05 10:17:54 +0200

    commit 1

--- TIC-TAC-TOE/src/main/java/com/TEAM4_C4/TIC_TAC_TOE/App.java
+++ TIC-TAC-TOE/src/main/java/com/TEAM4_C4/TIC_TAC_TOE/App.java
@@ -6,10 +6,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class App extends JFrame {
 
 	private JPanel contentPane;
+	private final ButtonGroup numeroJugadores = new ButtonGroup();
+	private JTextField tfNombreJ1;
+	private JTextField tfNombreJ2;
 
 	/**
 	 * Launch the application.
@@ -34,21 +41,41 @@ public class App extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setLayout(null);
 		setContentPane(contentPane);
+		
+		JRadioButton rdbtn1 = new JRadioButton("1 Jugador");
+		rdbtn1.setSelected(true);
+		numeroJugadores.add(rdbtn1);
+		rdbtn1.setBounds(89, 84, 109, 23);
+		contentPane.add(rdbtn1);
+		
+		JRadioButton rdbtn2 = new JRadioButton("2 Jugadores");
+		numeroJugadores.add(rdbtn2);
+		rdbtn2.setBounds(200, 84, 109, 23);
+		contentPane.add(rdbtn2);
+		
+		JLabel lblNewLabel = new JLabel("Nombre Jugador 1");
+		lblNewLabel.setBounds(89, 132, 88, 14);
+		contentPane.add(lblNewLabel);
+		
+		tfNombreJ1 = new JTextField();
+		tfNombreJ1.setBounds(89, 151, 86, 20);
+		contentPane.add(tfNombreJ1);
+		tfNombreJ1.setColumns(10);
+		
+		JLabel lblNewLabel2 = new JLabel("Nombre Jugador 2");
+		lblNewLabel2.setBounds(200, 132, 88, 14);
+		contentPane.add(lblNewLabel2);
+		
+		tfNombreJ2 = new JTextField();
+		tfNombreJ2.setEditable(false);
+		tfNombreJ2.setColumns(10);
+		tfNombreJ2.setBounds(200, 151, 86, 20);
+		contentPane.add(tfNombreJ2);
+		
+			//tfNombreJ2.setEditable(true);;
+		
+		
 	}
-	
-	//funcion turno
-	
-	//funcion colocar ficha
-	
-	//funcion se puede colocar
-	
-	//se cambia la casilla
-	
-	//se valida
-	
-	//gana o sigue
-
 }
