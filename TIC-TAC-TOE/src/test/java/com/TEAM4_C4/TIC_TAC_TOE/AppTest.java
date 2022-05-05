1f67d42e3dfe651b4fadc4efc7c6f07e46b43044
Eloi Martorell Martin <eloi.martin174@gmail.com>
2022-05-05 09:46:02 +0200

    commit maven

---
+++ TIC-TAC-TOE/src/test/java/com/TEAM4_C4/TIC_TAC_TOE/AppTest.java
@@ -0,0 +1,20 @@
+package com.TEAM4_C4.TIC_TAC_TOE;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
