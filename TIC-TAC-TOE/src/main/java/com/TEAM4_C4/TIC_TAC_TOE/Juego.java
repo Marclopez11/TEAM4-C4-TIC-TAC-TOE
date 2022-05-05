4a86932a875d19a78819895801b63a48179e37a6
Marc Lopez <marclopezmarco469@gmail.com>
2022-05-05 11:45:53 +0200

    Pantalla del juego empezada1.2

--- TIC-TAC-TOE/src/main/java/com/TEAM4_C4/TIC_TAC_TOE/Juego.java
+++ TIC-TAC-TOE/src/main/java/com/TEAM4_C4/TIC_TAC_TOE/Juego.java
@@ -16,6 +16,11 @@ import java.awt.event.MouseEvent;
 import javax.swing.JToggleButton;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
+import javax.swing.JTree;
+import javax.swing.JLabel;
+import javax.swing.JSplitPane;
+import javax.swing.JInternalFrame;
+import javax.swing.JProgressBar;
 
 public class Juego extends JFrame {
 
@@ -42,40 +47,55 @@ public class Juego extends JFrame {
 	 */
 	public Juego() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 600, 900, 900);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new GridLayout(3, 3, 0, 0));
+		contentPane.setLayout(null);
 		
 		final JToggleButton btn11 = new JToggleButton("");
-		btn11.setToolTipText("size=33");
+		btn11.setBounds(6, 5, 146, 87);
 		contentPane.add(btn11);
 		
 		JToggleButton btn12 = new JToggleButton("");
+		btn12.setBounds(152, 5, 146, 87);
 		contentPane.add(btn12);
 		
 		JToggleButton btn13 = new JToggleButton("");
+		btn13.setBounds(298, 5, 146, 87);
 		contentPane.add(btn13);
 		
 		JToggleButton btn21 =new JToggleButton("");
+		btn21.setBounds(6, 92, 146, 87);
 		contentPane.add(btn21);
 		
 		JToggleButton btn22 = new JToggleButton("");
+		btn22.setBounds(152, 92, 146, 87);
 		contentPane.add(btn22);
 		
 		JToggleButton btn23 = new JToggleButton("");
+		btn23.setBounds(298, 92, 146, 87);
 		contentPane.add(btn23);
 		
 		JToggleButton btn31 = new JToggleButton("");
-		btn31.setToolTipText("");
+		btn31.setBounds(6, 179, 146, 87);
 		contentPane.add(btn31);
 		
 		JToggleButton btn32 = new JToggleButton("");
+		btn32.setBounds(152, 179, 146, 87);
 		contentPane.add(btn32);
 		
 		JToggleButton btn33  =new JToggleButton(""); 
+		btn33.setBounds(298, 179, 146, 87);
 		contentPane.add(btn33);
+		
+		JButton btnNuevaPartida = new JButton("NuevaPartida");
+		btnNuevaPartida.setBounds(636, 16, 117, 29);
+		contentPane.add(btnNuevaPartida);
+		
+		JLabel lblNewLabel = new JLabel("X, coloca ficha...");
+		lblNewLabel.setBounds(489, 54, 61, 16);
+		contentPane.add(lblNewLabel);
 		
 		
 		
@@ -101,5 +121,4 @@ public class Juego extends JFrame {
 			
 		
 	}
-
 }
